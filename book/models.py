c2a4708b5051c8170ae226e74d81055641bf4326
dyakin.i <iigor93@gmail.com>
2023-04-21 13:57:57 +0300

    init

--- book/models.py
+++ book/models.py
@@ -8,7 +8,7 @@ class Book(models.Model):
         ordering = ('-id',)
 
     name = models.CharField(max_length=20, verbose_name="Название")
-    title = models.CharField(max_length=30, verbose_name="Заголовок")
+    title = models.CharField(max_length=30, verbose_name="Заголовок", null=True, blank=True)
     author = models.CharField(max_length=30, verbose_name="Автор")
     description = models.CharField(max_length=512, verbose_name="Описание", null=True, blank=True)
     price = models.PositiveIntegerField(verbose_name="Цена")
