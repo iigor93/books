6f385784d000ccbd3a2720d6803684fab466db57
igor <iigor93@gmail.com>
2023-04-21 18:27:30 +0300

    html

--- start_page/views.py
+++ start_page/views.py
@@ -2,11 +2,16 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
 
+from book.models import Book
+
 
 class StartPageView(View):
 
     template_name = "start_page/start_page.html"
 
     def get(self, request, *args, **kwargs):
-        context = {"welcome": "!welcome"}
+        books = Book.objects.all()
+        context = {"welcome": "!welcome",
+                   "books": books,
+                   }
         return render(request, self.template_name, context)
