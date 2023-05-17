644a44a49af61a2e4be92e1849cea7b4fd2aadd3
dyakin.i <iigor93@gmail.com>
2023-05-17 12:58:43 +0300

    actions

--- book/views.py
+++ book/views.py
@@ -1,11 +1,10 @@
 from rest_framework import viewsets
-
+from icecream import ic
 from book.models import Book
 from book.serializers import BookSerializer
 
 
 class BookListView(viewsets.ModelViewSet):
     """ Список книг """
-    static("geyt")
     queryset = Book.objects.all()
     serializer_class = BookSerializer
