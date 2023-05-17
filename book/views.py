c14563c146747e2d9aa8f3aceec0691896455493
dyakin.i <iigor93@gmail.com>
2023-05-17 11:30:57 +0300

    actions

--- book/views.py
+++ book/views.py
@@ -5,6 +5,6 @@ from book.serializers import BookSerializer
 
 
 class BookListView(viewsets.ModelViewSet):
-    """ Список книг """
+    """ Список книгicecreame """
     queryset = Book.objects.all()
     serializer_class = BookSerializer
